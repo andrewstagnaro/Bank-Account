f0a8777d9433d301a388f05655d1f6ee96d7b71d
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-09-26 13:38:13 -0400

    Add files via upload

---
+++ BankAccount/src/BankAccount.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @author andrew stagnaro
+ *
+ */
+	public class BankAccount 
+	{
+
+		private String name;
+		private int accNum;
+		private int balance;
+		
+		
+		public BankAccount()
+		{
+			name = "account1";
+		}
+		
+		
+		
+		public BankAccount()
+		{
+			accNum = 
+		}
+		
+		
+		
+
+	}
