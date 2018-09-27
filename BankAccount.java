b1842cab4028ca3fc3a2c3bca679573eaa4428d4
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-09-27 13:19:12 -0400

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ * @author andrew stagnaro
+ *
+ */
+	public class BankAccount 
+	{
+
+		private String name;
+		private int accNum;
+		private double balance;
+		
+		
+		public BankAccount(int acNum, String n)
+		{
+			balance = 0;
+			accNum = acNum;
+			name = n;
+		}
+		
+		public BankAccount(String n, int acNum, double bal)
+		{
+			name = n;
+			accNum = acNum;
+			balance = bal;
+		}
+		
+		public void withdraw(double amt)
+		{
+			balance -= amt;
+		}
+		
+		public void deposit(double amt)
+		{
+			balance += amt;
+		}
+	
+		public return getBalance()
+		{
+			return balance;
+		}
+		
+		public String toString()
+		{
+			return "name: " + 
+		}
+	
+	}
